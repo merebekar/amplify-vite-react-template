daba4150ee43afcc396d201dda0e315e7a50caed
amukheeth <mukheeth@alicehelps.com>
2025-02-10 20:36:56 -0500

    more changes

--- src/components/StorageBrowser.tsx
+++ src/components/StorageBrowser.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+/*import React from 'react';*/
 import { createAmplifyAuthAdapter, createStorageBrowser } from '@aws-amplify/ui-react-storage/browser';
 import '@aws-amplify/ui-react-storage/styles.css';
 import { Amplify } from 'aws-amplify';
-import config from '../amplify_outputs.json';
+import config from '../../amplify_outputs.json';
 
 // Configure Amplify using the imported configuration
 Amplify.configure(config);
